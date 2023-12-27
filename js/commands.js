6b2ccc636da68d32057bd306d252daea8269e2b9
AshenWijesingha <a.d.wijesingha@gmail.com>
2023-12-27 22:21:50 +0530

    Initial Commit

---
+++ js/commands.js
@@ -0,0 +1,133 @@
+var youtube = "#";
+var twitter = "#";
+var password = "Huh You got me dude ⛺";
+var linkedin = "https://www.linkedin.com/in/ashen-wijesingha/";
+var instagram = "https://www.instagram.com/ashen_wijesingha/";
+var github = "https://github.com/Falcon-98";
+var email = 'mailto:terminal@falcon98.com';
+var website = "https://falcon98.com/";
+
+var pfacebook = "https://www.facebook.com/ashen.dilshan.96/";
+var plinkedin = "https://www.linkedin.com/in/ashen-wijesingha/";
+var pinstagram = "https://www.instagram.com/ashen_wijesingha/";
+var pgithub = "https://github.com/AshenWijesingha";
+var pemail = 'mailto:inbox.ashen@gmail.com';
+var pcontact = 'tel:+94784464128';
+var pwebsite = 'https://ashenwijesingha.github.io';
+
+var prgQRCode = "https://ashenwijesingha.github.io/QR-Code-Generator/";
+
+var qr = "https://ashenwijesingha.github.io/QR-Code-Generator/";
+
+
+
+whois = [
+  "<span class=\"text-container\">",
+  "<br>",
+  "Hey, I'm Falocon98!👋",
+  "<br>",
+  "Falcon 98 is a pioneering technology services provider, dedicated to guiding businesses through the intricacies of the digital era.",
+  "Our commitment to innovation, coupled with a focus on excellence and customer satisfaction, sets us apart. We seamlessly integrate",
+  "cutting-edge technology with industry expertise to empower organizations for success. Recognizing the importance of a holistic approach,",
+  "our seasoned professionals offer tailored solutions, considering the unique challenges and goals of each client. At the core of our",
+  "values is a dedication to fostering lasting partnerships, ensuring that our clients receive not only state-of-the-art technology",
+  "solutions but also strategic insights. We remain at the forefront of industry trends through continuous investment in research and",
+  "development, offering clients the latest innovations to stay ahead in their markets. Falcon 98 invites businesses to collaborate in",
+  "the pursuit of digital excellence, providing the tools and strategies necessary for success in today's fast-paced technological landscape.",
+  "<br>",
+  "</span>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/falcon98' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/falcon98' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/falcon98' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/falcon98' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/falcon98' + "</a>",
+  'website        <a href="' + website + '" target="_blank">falcon98.com' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  'QR Code Generator       <a href="' + prgQRCode + '" target="_blank">QR Code Generator' + "</a>",
+  "Still curating... most projects are offline, on GitHub, or confidential. 🫂",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Falcon 98?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">Q or q</span>         Ask me a question',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Send an email to me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">contact</span>        How to contact the person who made this',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Terminal by Falcon 98 Corporation. All rights reserved.</span>',
+  "<br>",
+  "      ooooooooooooooooooooooooooooooooooooo",
+  "      8                                .d88",
+  "      8  oooooooooooooooooooooooooooood8888",
+  '      8  8888888888888888888888888P"   8888    oooooooooooooooo',
+  '      8  8888888888888888888888P"      8888    8              8',
+  '      8  8888888888888888888P"         8888    8 Falcon 98 ❤️d8',
+  '      8  8888888888888888P"            8888    8  Terminal  d88',
+  '      8  8888888888888P"               8888    8           d888',
+  '      8  8888888888P"                  8888    8          d8888',
+  '      8  8888888P"                     8888    8         d88888',
+  '      8  8888P"                        8888    8        d888888',
+  "      8  8888oooooooooooooooooooooocgmm8888    8       d8888888",
+  "      8 .od88888888888888888888888888888888    8      d88888888",
+  "      8888888888888888888888888888888888888    8     d888888888",
+  "                                               8    d8888888888",
+  "         ooooooooooooooooooooooooooooooo       8   d88888888888",
+  "        d                       ...oood8b      8  d888888888888",
+  "       d              ...oood888888888888b     8 d8888888888888",
+  "      d     ...oood88888888888888888888888b    8d88888888888888",
+  "     dood8888888888888888888888888888888888b   8888888888888888",
+  "<br>",
+  '<span class="color2">Welcome to Falcon 98 Corporation\'s interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  "<br>",
+];
+
+helpqna = [
+  "<br>",
+  '<span class="command">contact</span>          How to contact me?',
+  "<br>",
+]
+
+contact = [
+  "<br>",
+  "Contact me via:",
+  'facebook       <a href="' + pfacebook + '" target="_blank">facebook/AshenWijesingha' + '</a>',
+  'linkedin       <a href="' + plinkedin + '" target="_blank">linkedin/AshenWijesingha' + "</a>",
+  'instagram      <a href="' + pinstagram + '" target="_blank">instagram/AshenWijesingha' + '</a>',
+  'github         <a href="' + pgithub + '" target="_blank">github/AshenWijesingha' + "</a>",
+  'website        <a href="' + pwebsite + '" target="_blank">ashenwijesingh.github.io' + "</a>",
+  'email          <a href="' + pemail + '" target="_blank">inbox.ashen@gmail.com' + "</a>",
+  'contact        <a href="' + pcontact + '" target="_blank">+94 78 446 4128' + "</a>",
+  "<br>",
+]
